88af0013b29b1096f0b4b937938323b1d73dc56d
LuongVitEnvorso <vito.luong@envorso.com>
2023-09-14 15:41:34 -0700

    update

--- app.ts
+++ app.ts
@@ -1,15 +1,15 @@
 // app.ts
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express TypeScript World!');
+app.get("/", (req, res) => {
+	res.send("Hello, Express TypeScript World!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
 
 export default app;
